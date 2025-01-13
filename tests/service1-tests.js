8adcede77f6915f63ceba90d4605bf647d497a4b
Olli Lehmuskenttä <olli.lehmuskentta@gmail.com>
2025-01-13 16:56:14 +0000

    Test ci testi 6

--- tests/service1-tests.js
+++ tests/service1-tests.js
@@ -1,12 +1,26 @@
 const assert = require('assert');
-const fetch = require('node-fetch');
+const http = require('http'); 
 
 describe('Service1 API', function () {
-    it('should return service1 info', async function () {
-        const response = await fetch('http://localhost:8197/service1');
-        const data = await response.json();
-        assert.equal(response.status, 200);
-        assert.ok(data.service1);
-        assert.ok(data.service2);
+    it('should return service1 info', function (done) {
+        http.get('http://localhost:8197/service1', (response) => {
+            let data = '';
+            
+
+            response.on('data', (chunk) => {
+                data += chunk;
+            });
+
+
+            response.on('end', () => {
+                const parsedData = JSON.parse(data);
+                assert.equal(response.statusCode, 200);
+                assert.ok(parsedData.service1);
+                assert.ok(parsedData.service2);
+                done();
+            });
+        }).on('error', (err) => {
+            done(err); 
+        });
     });
 });
