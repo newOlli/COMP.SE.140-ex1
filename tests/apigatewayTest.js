3607362017ca6409869088d7dd7fe6fe50fff275
Olli Lehmuskenttä <olli.lehmuskentta@gmail.com>
2025-01-15 11:48:18 +0000

    Trying to run tests in its own container connected to the network to not have the network errors on ci pipeline tests.

--- tests/apigatewayTest.js
+++ tests/apigatewayTest.js
@@ -6,7 +6,7 @@ function httpRequest({ method, path, payload = null, headers = {} }) {
   return new Promise((resolve, reject) => {
     const options = {
       method,
-      hostname: 'localhost',
+      hostname: 'apigateway',
       port: 8197,
       path,
       headers,
